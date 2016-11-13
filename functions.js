edcee07ca35ba7336508ea7a25084c52ccf55471
garyuu <shuhanrpg@gmail.com>
2016-11-13 16:30:08 +0800

    Initialize functions.js

---
+++ functions.js
@@ -0,0 +1,34 @@
+function Clear()
+{
+    
+}
+
+function Cancel()
+{
+
+}
+
+function BackSpace()
+{
+
+}
+
+function Operator(op)
+{
+
+}
+
+function Value(val)
+{
+
+}
+
+function Negative()
+{
+
+}
+
+function Calculate()
+{
+
+}
